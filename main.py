2f6198e1a78cbbdaf000369c0d3e819d6bc1c907
Uczeń <Ucze@ZSO1_1>
2023-10-11 12:17:27 +0200

    saffea

---
+++ main.py
@@ -0,0 +1,17 @@
+liczba = int(input('Podaj liczbę od 1 do 5'))
+print(type(liczba))
+
+
+if liczba == 1:
+    print('jeden')
+elif liczba == 2:
+    print('dwa')
+elif liczba == 3:
+    print('trzy')
+elif liczba == 4:
+    print('cztery')
+elif liczba == 5:
+    print('pięć')
+else:
+    print('Czytaj uważniej polecenia!')
+print('koniec')
